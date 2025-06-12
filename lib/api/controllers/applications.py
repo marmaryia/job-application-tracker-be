10783c14d99e35dc1353754420d01fce208e0d2f
marmaryia <marozavamaryia@gmail.com>
2025-06-12 15:21:14 +0100

    add error handling for non-existent id

--- lib/api/controllers/applications.py
+++ lib/api/controllers/applications.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, request
 from sqlalchemy import asc, desc
 
-from lib.db.models import Application, Event
+from extensions import db
+from lib.db.models import Application, Event, User
 from lib.db.schemas import applications_schema
+from lib.api.controllers.exceptions import ResourceNotFoundError
 
 applications_bp = Blueprint("applications", __name__)
 
 @applications_bp.get("/<int:user_id>/applications")
 def get_applications_by_user_id(user_id):
     
+    if not User.query.filter_by(id=user_id).first():
+        raise ResourceNotFoundError
+
     order = request.args.get("order", "desc")
     sort_by = request.args.get("sort_by", "date_created")
 
