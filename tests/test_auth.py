44edee11e66d4e083940c80d0b082b127fd2f187
marmaryia <marozavamaryia@gmail.com>
2025-08-14 15:10:11 +0100

    add tests for /login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -50,7 +50,7 @@ def test_register_incomplete_data(app):
     assert response.status_code == 400
     assert response.json == {"message": "Bad Request"}
 
-def test_register_incomplete_data(app):
+def test_register_invalid_data(app):
     """
     Returns a 400 error if a data object with invalid data is provided.
     """
@@ -77,3 +77,55 @@ def test_register_incomplete_data(app):
     response = app.post("api/auth/register", json={"name": "name", "email": "user123@email.com", "password": "12 34567"})
     assert response.status_code == 400
     assert response.json == {"message": "Bad Request: Invalid Input"}
+
+def test_login_success(app, mocker):
+    """
+    Accepts a request body with email and password and returns a JSON object with the logged in user and the access token.
+    """
+    mocked_token_creator = mocker.patch("lib.api.controllers.auth.create_access_token", return_value = "access_token")
+    
+    response = app.post("api/auth/login", json={"email": "user1@email.com", "password": "123456"})
+    assert response.status_code == 200
+    assert response.json["user"] == {"name": "user1", "email": "user1@email.com", "id": 1}
+    assert response.json["access_token"] == "access_token"
+    mocked_token_creator.assert_called_once()
+
+def test_login_incorrect_password(app):
+    """
+    Returns an authentication 401 error if the password provided is incorrect. 
+    """
+    response = app.post("api/auth/login", json={"email": "user1@email.com", "password": "12345678"})
+    assert response.status_code == 401
+    assert response.json["message"] == "Authentication Failed"
+    
+def test_login_nonexistent_user(app):
+    """
+    Returns a 404 error if no user with the provided email address. 
+    """
+    response = app.post("api/auth/login", json={"email": "user123@email.com", "password": "123456"})
+    assert response.status_code == 404
+    assert response.json["message"] == "Resource not found"
+
+def test_login_incomplete_data(app):
+    """
+    Returns a 400 error if the data object provided is incomplete. 
+    """
+    response = app.post("api/auth/login", json={"password": "123456"})
+    assert response.status_code == 400
+    assert response.json["message"] == "Bad Request"
+
+    response = app.post("api/auth/login", json={"email": "user1@email.com"})
+    assert response.status_code == 400
+    assert response.json["message"] == "Bad Request"
+
+def test_login_invalid_data(app):
+    """
+    Returns a 400 error if the data object provided is invalid. 
+    """
+    response = app.post("api/auth/login", json={"email": "user1@email.com", "password": ""})
+    assert response.status_code == 400
+    assert response.json["message"] == "Bad Request"
+
+    response = app.post("api/auth/login", json={"email": "", "password": "123456"})
+    assert response.status_code == 400
+    assert response.json["message"] == "Bad Request"
